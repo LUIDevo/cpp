37104204d29accf71468bb61240f2a507fa59a50
louis <sanicisspeedandfast@gmail.com>
2026-07-16 19:35:06 -0400

    resetting

--- main.cpp
+++ main.cpp
@@ -1,28 +1,9 @@
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int add (int a, int b) {
-	return a+b;
-}
+#include <chrono>
+auto t0 = std::chrono::steady_clock::now();
 
-int main() {
-	int x = 1, y=2;
-	int arr[2] = {1,2};
-	if (x < y) {
-		cout << "Y IS BIGGER" << endl;
-	}
-	cout << add(x,y) << endl;
-	cin >> x;
-	cout << "Hello World!" << endl;
-	std::vector<int> dynamicArray(5);
-	for (int i=0; i<5; i++) {
-		cout << dynamicArray[i];
-	}
-	cout << add(x,y) << endl;
-	for (int i=0; i<5; i++) {
-		cout << "HI";
-	}
-	return 0;
-}
+auto t1 = std::chrono::steady_clock::now();
+auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
