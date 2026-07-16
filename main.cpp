07ca0cbbdfed2980307b9172618b32b0726e91b0
louis <sanicisspeedandfast@gmail.com>
2026-07-15 23:37:52 -0400

    functions

--- main.cpp
+++ main.cpp
@@ -2,13 +2,19 @@
 #include <iostream>
 using namespace std;
 
+int add (int a, int b) {
+	return a+b;
+}
+
 int main() {
-	int x = 1;
-	int y = 1;
-	cout << x+y << endl;
+	int x = 1, y=2;
+	if (x < y) {
+		cout << "Y IS BIGGER" << endl;
+	}
+	cout << add(x,y) << endl;
 	cin >> x;
 	cout << "Hello World!" << endl;
-	cout << x+y << endl;
+	cout << add(x,y) << endl;
 	for (int i=0; i<5; i++) {
 		cout << "HI";
 	}
