79a9f2b45124b9a87ca57d48994828b7a2703a3f
louis <sanicisspeedandfast@gmail.com>
2026-07-16 20:00:52 -0400

    loop

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 #include <chrono>
-auto t0 = std::chrono::steady_clock::now();
-
-auto t1 = std::chrono::steady_clock::now();
-auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
+int main() {
+	for (int i=0; i<10; i++) {
+		auto t0 = std::chrono::steady_clock::now();
+		for (long j=0; j<1000000; j++) {
+			cout << j+2 << endl;
+		}
+		auto t1 = std::chrono::steady_clock::now();
+		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
+	}
+	return 0;
+}
