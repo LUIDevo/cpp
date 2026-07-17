64de3c6fb941481ae8ee25bbe1fc37cfa15c4457
louis <sanicisspeedandfast@gmail.com>
2026-07-16 20:13:20 -0400

    u

--- main.cpp
+++ main.cpp
@@ -4,13 +4,18 @@ using namespace std;
 
 #include <chrono>
 int main() {
+	vector<double> results = {};
 	for (int i=0; i<10; i++) {
 		auto t0 = std::chrono::steady_clock::now();
 		for (long j=0; j<1000000; j++) {
-			cout << j+2 << endl;
+			long x = j+2;
 		}
 		auto t1 = std::chrono::steady_clock::now();
-		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
+		auto ms = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
+		results.push_back(ms);
+	}
+	for (double result : results) {
+		cout << result << endl;
 	}
 	return 0;
 }
