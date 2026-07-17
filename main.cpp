710423f1504faa19a90f473310547f0b5351ea77
louis <sanicisspeedandfast@gmail.com>
2026-07-17 17:02:45 -0400

    abstracted print_stats

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 using namespace std;
 
 struct BenchResult {
@@ -7,22 +8,26 @@ struct BenchResult {
 	vector<double> times_ms;
 };
 
-#include <chrono>
+void print_stats(const BenchResult & r) {
+	for (double result : r.times_ms) {
+		cout << result << endl;
+	}
+}
+
 int main() {
-	vector<double> results = {};
+	vector<double> times = {};
+	BenchResult results ("sum", times);
 	for (int i=0; i<10; i++) {
 		auto t0 = std::chrono::steady_clock::now();
 		long long sum = 0;
 		for (long j=0; j<1000000; j++) {
 			sum += j+2;
 		}
-		cout << "sum: " << sum << "\n";
 		auto t1 = std::chrono::steady_clock::now();
 		auto ms = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
-		results.push_back(ms);
+		cout << "sum: " << sum << "\n";
+		results.times_ms.push_back(ms);
 	}
-	for (double result : results) {
-		cout << result << endl;
-	}
+	print_stats(results);
 	return 0;
 }
