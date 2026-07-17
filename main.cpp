a9e0e60357516d1a3fb5fa881ec0da14084300bc
louis <sanicisspeedandfast@gmail.com>
2026-07-16 20:41:18 -0400

    struct

--- main.cpp
+++ main.cpp
@@ -2,14 +2,21 @@
 #include <vector>
 using namespace std;
 
+struct BenchResult {
+	string name;
+	vector<double> times_ms;
+};
+
 #include <chrono>
 int main() {
 	vector<double> results = {};
 	for (int i=0; i<10; i++) {
 		auto t0 = std::chrono::steady_clock::now();
+		long long sum = 0;
 		for (long j=0; j<1000000; j++) {
-			long x = j+2;
+			sum += j+2;
 		}
+		cout << "sum: " << sum << "\n";
 		auto t1 = std::chrono::steady_clock::now();
 		auto ms = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
 		results.push_back(ms);
